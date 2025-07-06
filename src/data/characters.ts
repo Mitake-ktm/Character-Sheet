9e376c46f606fcfb8ad34070c365ae78566a4f1e
Mitake-ktm <kev800da@gmail.com>
2025-07-06 14:01:18 +0200

    Data of characters

---
+++ src/data/characters.ts
@@ -0,0 +1,15 @@
+export const characters = [
+    {
+        id: "mike",
+        name: "Mike",
+        title: "Jeune voulant survivre",
+        quote: "La voix en moi... mais qui est-elle ?",
+        description: "Mike est un jeune homme perdu dans un monde étrange. Entendant des voix...",
+        image: "/images/mike.png",
+        details: {
+            age: "16 ans",
+            size: "1m71",
+            personnalité: ["Introverti","Curieux","Tourmenté"]
+        }
+    }
+]
